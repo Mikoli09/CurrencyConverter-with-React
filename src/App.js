0a76d6aad1d05491881518721376d6cb6503b060
Marcin Osojca <osojca.marcin1@gmail.com>
2025-01-30 14:00:39 +0100

    css and title added, section to index.html added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+        <header className="section__header">Kalkulator walutowy</header>
+        <form>
+            <p className="form__paragraph">Chcę przeliczyć
+                <select className="form__select">
+                    <option>EUR</option>
+                    <option>USD</option>
+                    <option>PLN</option>
+                </select>
+                na
+                <select className="form__select">
+                    <option>EUR</option>
+                    <option>USD</option>
+                    <option>PLN</option>
+                </select>
+            </p>
+            <p className="form__paragraph">
+                <label for="amount"><span className="form__label">Wprowadź kwotę do przeliczenia:</span></label>
+                <input className="form__amount js-form__amount" type="number" step="0.01" name="amount" id="amount" min="0"
+                    max="1000000000" required placeholder="Ilość pieniędzy"/>
+            </p>
+            <p>
+                <button className="form__button">Przelicz</button>
+            </p>
+            <p className="form__resultParagraph"></p>
+        </form>
+    </section>
   );
 }
 
