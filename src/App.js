02a8f2b9b9aaf90a0d6302ca94fc272699770675
Marcin Osojca <osojca.marcin1@gmail.com>
2025-01-30 18:03:12 +0100

    currencies structure created

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 
-
+import Header from "./Header";
+import Form from "./Form";
 
 
 
 function App() {
+
+  const currencies = [
+    { id: 1, name: "EUR", toSubcurrencyConverter: 1, toFinalCurrencyConverter: 1 },
+    { id: 2, name: "USD", toSubcurrencyConverter: 0.9022, toFinalCurrencyConverter: 1.1084 },
+    { id: 3, name: "PLN", toSubcurrencyConverter: 0.2359, toFinalCurrencyConverter: 4.2383 },
+  ];
+
+
+
+
+
+
   return (
     <section className="section">
-        <header className="section__header">Kalkulator walutowy</header>
-        <form>
-            <p className="form__paragraph">Chcę przeliczyć
-                <select className="form__select">
-                    <option>EUR</option>
-                    <option>USD</option>
-                    <option>PLN</option>
-                </select>
-                na
-                <select className="form__select">
-                    <option>EUR</option>
-                    <option>USD</option>
-                    <option>PLN</option>
-                </select>
-            </p>
-            <p className="form__paragraph">
-                <label for="amount"><span className="form__label">Wprowadź kwotę do przeliczenia:</span></label>
-                <input className="form__amount js-form__amount" type="number" step="0.01" name="amount" id="amount" min="0"
-                    max="1000000000" required placeholder="Ilość pieniędzy"/>
-            </p>
-            <p>
-                <button className="form__button">Przelicz</button>
-            </p>
-            <p className="form__resultParagraph"></p>
-        </form>
+      <Header title={"Kalkulator walutowy"} />
+      <Form currencies={currencies}  />
     </section>
   );
 }
