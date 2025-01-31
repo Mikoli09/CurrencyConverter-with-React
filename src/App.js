e9b0861d2f89ff6eec00a38b340e1c294e9d0e38
Marcin Osojca <osojca.marcin1@gmail.com>
2025-01-31 13:27:23 +0100

    component section created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-
-import Header from "./Header";
-import Form from "./Form";
-
-
+import currencies from "./assets";
+import Section from "./Section";
 
 function App() {
 
-  const currencies = [
-    { id: 1, name: "EUR", toSubcurrencyConverter: 1, toFinalCurrencyConverter: 1 },
-    { id: 2, name: "USD", toSubcurrencyConverter: 0.9022, toFinalCurrencyConverter: 1.1084 },
-    { id: 3, name: "PLN", toSubcurrencyConverter: 0.2359, toFinalCurrencyConverter: 4.2383 },
-  ];
-
-
-
-
-
-
   return (
-    <section className="section">
-      <Header title={"Kalkulator walutowy"} />
-      <Form currencies={currencies}  />
-    </section>
+    <Section currencies={currencies} />
   );
 }
 
