23e9a95ac8a4ed4266a523679cf77636752cdf8a
Marcin Osojca <osojca.marcin1@gmail.com>
2025-01-31 13:22:47 +0100

    currencies moved to assets file

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+
+const currencies = [
+    { id: 0, name: "" },
+    { id: 1, name: "EUR", toSubcurrencyConverter: 1, toFinalCurrencyConverter: 1 },
+    { id: 2, name: "USD", toSubcurrencyConverter: 0.9022, toFinalCurrencyConverter: 1.1084 },
+    { id: 3, name: "PLN", toSubcurrencyConverter: 0.2359, toFinalCurrencyConverter: 4.2383 },
+  ];
+
+  export default currencies;
