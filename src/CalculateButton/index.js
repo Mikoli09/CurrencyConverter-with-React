66d485d6f432539228922d524990fe503fc2eadf
Marcin Osojca <osojca.marcin1@gmail.com>
2025-02-05 09:13:53 +0100

    CalculateButton component rebuild

--- src/CalculateButton/index.js
+++ src/CalculateButton/index.js
@@ -2,13 +2,11 @@ import "./style.css"
 
 const CalculateButton = ({ currencyFrom, currencyTo }) => {
 
-    const buttonActive = () => currencyFrom.id !== 0 && currencyTo.id !== 0;
-
     return (
         <p className="form__paragraph">
             <button
                 className="form__button"
-                disabled={!buttonActive()}
+                disabled={currencyFrom.id === 0 || currencyTo.id === 0}
             >
                 Convert
             </button>
