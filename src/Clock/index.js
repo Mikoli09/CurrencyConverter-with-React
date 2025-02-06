2de496cabd91e0c7ed2a5047f3807c131b102c23
Marcin Osojca <osojca.marcin1@gmail.com>
2025-02-06 20:04:20 +0100

    useCurrentDate hook added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,27 +1,9 @@
 
-import { useEffect, useState } from "react";
 import "./style.css";
+import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = () => {
-
-    const [actualDate, setActualDate] = useState(new Date());
-    const dateString = actualDate.toLocaleString(
-        undefined, {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
-    })
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setActualDate(new Date())
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
+    const { dateString } = useCurrentDate();
 
     return (
         <aside className="section__aside">
---
+++ src/Clock/useCurrentDate.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import { useState } from "react";
+
+export const useCurrentDate = () => {
+    const [actualDate, setActualDate] = useState(new Date());
+    const dateString = actualDate.toLocaleString(
+        undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    })
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActualDate(new Date())
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+    return {dateString}
+};
